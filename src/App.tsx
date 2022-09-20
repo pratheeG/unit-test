567ad11ab8174fa26e5652aee9680a98c95136bf
pratheeG <premsuryamj@gmail.com>
2022-09-20 11:45:10 +0530

    Update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import './App.css';
+
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn JS
+          Learn React
         </a>
       </header>
     </div>
